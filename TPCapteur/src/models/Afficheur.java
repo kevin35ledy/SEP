c9e4d7436638387028d83bb0059708bb701231fd
kevin <kevin.ledy@gmail.com>
2017-01-05 11:08:43 +0100

    ajout des tests

--- TPCapteur/src/models/Afficheur.java
+++ TPCapteur/src/models/Afficheur.java
@@ -25,8 +25,6 @@ public class Afficheur implements ObserverDeCapteur {
 
 	@Override
 	public Future<?> updateFuture(Capteur canal) {
-		// TODO Auto-generated method stub
-//		System.out.println("Afficheur.update -> creation FUTURE "+this);
 		Future<Integer> f = ((Canal)canal).createGetValue();
 		try {
 			this.val = f.get().intValue();
@@ -34,7 +32,6 @@ public class Afficheur implements ObserverDeCapteur {
 			MaFenetre.setAfficheurValue(this.canal.getIdCapteur(),String.valueOf(this.val));
 			System.out.println("Afficheur"+this.canal.getIdCapteur()+" : "+ this.val);
 		} catch (NumberFormatException | InterruptedException | ExecutionException e) {
-//			System.err.println(e);
 			System.err.println("Afficheur. updateFuture -> formattage string to int mauvais");
 		}
 		return f;
@@ -42,7 +39,6 @@ public class Afficheur implements ObserverDeCapteur {
 
 	@Override
 	public void update(Capteur subject) {
-		// TODO Auto-generated method stub
 		
 	}
 
---
+++ TPCapteur/src/test/ObserverTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ObserverTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void test1() {
+		assertTrue(true);
+	}
+}
